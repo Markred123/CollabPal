f7296322e468a44b8efa589f844c62c4a0ef1d26
Markred123 <markred123@gmail.com>
2021-05-31 17:47:14 +0100

    LoginController

--- resources/views/registration.blade.php
+++ resources/views/registration.blade.php
@@ -12,44 +12,63 @@
     <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 </head>
 <body>
-    <div class="col-md-4 col-md-offset-4">
+<div class = "relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0 float-right">
 
-                <div class ="panel panel-default">
-                    <div class="panel-heading">
-                        <div class="panel-body">
-                            <form method="post" action="/register">
-                                @csrf
+    <a href="/registration">Home</a>
+    @if (!Auth::guest())
+        <a href="#test">Log Out</a>
+    @else
+        <a href="#test">Registration</a>
+        <a href="#test">Login</a>
+    @endif
 
-                                <div class="col-xs-6 col-sm-6 col-md-6">
 
-                                    <div class="form-group">
-                                        <input required type ="text" id="name" name="name" class="form-control input-sm" placeholder="Name"><br>
-                                    </div>
-                                </div>
-                                <div class="col-xs-6 col-sm-6 col-md-6">
-                                    <div class="form-group">
-                                        <input required type ="email" id="email" name="email" class="form-control input-sm" placeholder="Email"><br>
-                                    </div>
-                                </div>
-                                <div class="col-xs-6 col-sm-6 col-md-6">
-                                    <div class="form-group">
 
-                                        <input required type ="password" id="password" name="password" class="form-control input-sm" placeholder="Password"><br>
-                                    </div>
-                                </div>
-                                <div class="col-xs-6 col-sm-6 col-md-6">
-                                    <div class="form-group">
-                                        <input required type ="password" class="form-control input-sm" id="password_confirmation" name="password_confirmation"  placeholder="Confirmation"><br>
-                                    </div>
-                                </div>
-                                <input type="submit" value="Register" class="btn btn-info btn-block">
+</div>
+    <div class="col-md-3 col-md-offset-4 container ">
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
 
-                            </form>
+        <div class ="container col-md-12">
+        <form class="card p-3 bg-light " method="post" action="/register">
+            @csrf
+            <div class = "container  col-lg-12">
+            <div class="col-xs-6 col-sm-6 col-md-6 align-content-center">
 
-                        </div>
-                    </div>
-
+                <div class="form-group">
+                    <input required type ="text" id="name" name="name" class="form-control input-sm" placeholder="Name"><br>
+                </div>
             </div>
+            <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+                <div class="form-group">
+                    <input required type ="email" id="email" name="email" class="form-control input-sm" placeholder="Email"><br>
+                </div>
+            </div>
+            <div class="col-xs-6 col-sm-6 col-md-6">
+                <div class="form-group">
+
+                    <input required type ="password" id="password" name="password" class="form-control input-sm" placeholder="Password"><br>
+                </div>
+            </div>
+            <div class="col-xs-6 col-sm-6 col-md-6 ">
+                <div class="form-group">
+                    <input required type ="password" class="form-control input-sm" id="password_confirmation" name="password_confirmation"  placeholder="Confirmation"><br>
+                </div>
+            </div>
+            </div>
+            <input type="submit" value="Register" class="btn btn-info btn-block">
+
+        </form>
+        </div>
+
 
 
 
