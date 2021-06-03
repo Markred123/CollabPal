142807a7f5baa7bb842334aaa6d7f76c872fcdce
Markred123 <markred123@gmail.com>
2021-06-03 23:05:32 +0100

    Database started + User upload to folder S3

--- app/Http/Controllers/FileUploadController.php
+++ app/Http/Controllers/FileUploadController.php
@@ -5,7 +5,9 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Storage;
+use App\User;
 use Illuminate\Support\Facades\Auth;
+use App\Models\DbFile;
 
 class FileUploadController extends Controller
 {
@@ -15,13 +17,18 @@ class FileUploadController extends Controller
         return view('FileUpload');
     }
 
-    public function update(Request $request)
+    public function update(Request $request): \Illuminate\Http\RedirectResponse
     {
         $id = Auth::id();
         $file = $request->file('file');
-            $fileName=time().$file->getClientOriginalName();
-            $filePath = "files/$id/" . $fileName;
-            Storage::disk('s3')->put($filePath, file_get_contents($file));
-            return back()->with('success','You have successfully upload image.');
+        $fileName=time().$file->getClientOriginalName();
+        $filePath = "files/$id/" . $fileName;
+        Storage::disk('s3')->put($filePath, file_get_contents($file));
+
+        $user= Auth::user();
+
+        $dbFile= new Dbfile (['FileName'=>"$fileName",'FilePath'=>"$filePath"]);
+        $user->dbfiles()->save($dbFile);
+        return back()->with('Success','File Uploaded.');
     }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Fortify\TwoFactorAuthenticatable;
+use App\Models\DbFile;
 
 class User extends Authenticatable
 {
@@ -46,7 +47,7 @@ class User extends Authenticatable
         $this->attributes['password'] = bcrypt($password);
     }
 
-    public function filess(){
-        return $this->hasMany('app\Files');
+    public function dbfiles(){
+        return $this->hasMany('App\Models\DbFile');
     }
 }
