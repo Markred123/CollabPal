f30be284700e13ab6274a069b7349f37df70f940
Markred123 <markred123@gmail.com>
2021-06-12 00:48:06 +0100

    Folder system working - NEED TO MAKE IT SO DELETING FOLDER DELETES ALL FILES IN FOLDER

--- resources/views/FileFolders.blade.php
+++ resources/views/FileFolders.blade.php
@@ -15,17 +15,10 @@
                     <form method="post" action="/newFolder">
                         @csrf
                         <label>Create Folder</label>
-                        <input type="text" id="folderName" name="folderName">
+                        <input type="text" id="folderName" name="folderName" required>
                         <input type="submit" value="Create">
                     </form>
                 </div>
-                @if(session()->has('success'))
-                    <div class="alert alert-success">
-                        {{ session()->get('success') }}
-                    </div>
-                @elseif(session('error'))
-                    <div class="alert alert-danger">{{ session('error') }}</div>
-                @endif
                 @foreach($userFolder as $folder)
                     <div class="container">
                         <a href="{{url('/myFiles',$folder->id)}}">{{$folder->FolderName}} </a>
@@ -40,6 +33,40 @@
         </div>
 
 
+    </div>
+
+    <div class="container">
+
+
+        <div class="perfect-centering">
+            @if(Auth::User()->subscribed('Premium Collabpal'))
+                <form  action="{{ url('FileUpload') }}" method="post" enctype="multipart/form-data">
+                    @csrf
+                    <input type="file" name="file" id="file" required="required">
+                    <button type="submit">Upload</button>
+
+                    <br>
+                    <label>Folder:</label>
+                    <select id="folders" name="folders" required>
+                        @foreach($userFolder as $folder)
+                            <option value="{{$folder->id}}">{{$folder->FolderName}}</option>
+                        @endforeach
+                    </select>
+                </form>
+
+
+            @else
+                <p>Please subscribe to use premium features</p>
+            @endif
+
+            @if(session()->has('success'))
+                <div class="alert alert-success">
+                    {{ session()->get('success') }}
+                </div>
+            @elseif(session('error'))
+                <div class="alert alert-danger">{{ session('error') }}</div>
+            @endif
+        </div>
     </div>
 
 @endsection
